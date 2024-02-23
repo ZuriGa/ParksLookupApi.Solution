ec421e59116c617080325a4cc7aa4205d4fc15cc
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-23 15:39:33 -0800

    add park model + file set up

---
+++ ParksLookup/Models/Park.cs
@@ -0,0 +1,11 @@
+namespace ParksLookup.Models
+{
+  public class Park 
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string ParkType { get; set; }
+
+  }
+}
