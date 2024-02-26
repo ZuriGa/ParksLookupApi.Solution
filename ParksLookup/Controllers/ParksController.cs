79932c4b1a2b592f409e8b41fa8f5bf35d02c2ad
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-25 18:44:50 -0800

    add updated functionality

--- ParksLookup/Controllers/ParksController.cs
+++ ParksLookup/Controllers/ParksController.cs
@@ -44,5 +44,40 @@ namespace ParksLookup.Controllers
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
     }
+
+    //PUT: api/Parks/4
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Parks.Update(park);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else 
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
+    }
   }
 }
